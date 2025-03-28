bf0cefc098f6b00f4b341abb45af635f5a0fbc1d
KimNguyenTrong <kipmap123@gmail.com>
2025-03-28 15:51:42 +0700

    setup nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Nav } from "@/components/Nav/nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <title>Foso Soft</title>
       </head>
       <body>
+        <Nav />
         {children}
       </body>
     </html>
