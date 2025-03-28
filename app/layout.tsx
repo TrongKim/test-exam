f98396e24ac8e3902fea0e35735c0d9f92427a94
KimNguyenTrong <kipmap123@gmail.com>
2025-03-28 14:28:28 +0700

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Foso Soft</title>
+      </head>
+      <body>
         {children}
       </body>
     </html>
