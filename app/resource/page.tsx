cc24906f95080efa94893ecb71f3f8d6d2d1f74a
KimNguyenTrong <kipmap123@gmail.com>
2025-03-28 17:42:11 +0700

    add post display to page

--- app/resource/page.tsx
+++ app/resource/page.tsx
@@ -1,10 +1,12 @@
 import { BannerInformation } from '@/components/BannerInformation/banner_information';
+import { PostsDisplay } from '@/components/PostsDisplay/posts_display';
 import React from 'react'
 
 export const page = () => {
   return (
     <div>
         <BannerInformation />
+        <PostsDisplay />
     </div>
   )
 }
