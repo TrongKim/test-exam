927f3a35e0db8e644ccf6155e99f68f3e442c670
KimNguyenTrong <kipmap123@gmail.com>
2025-03-29 11:41:39 +0700

    fix eslint

--- app/resource/page.tsx
+++ app/resource/page.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 export const page = () => {
   return (
     <div>
-        {/* <BannerInformation />
-        <PostsDisplay /> */}
     </div>
   )
 }
